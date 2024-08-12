5f5f4053a571481bff5635c9dda98184dcf74b91
curiousKidd <skasrjhaan@gmail.com>
2024-08-12 23:12:29 +0900

    python code update

--- test.py
+++ test.py
@@ -1,27 +1,21 @@
-# pip install requests beautifulsoup4
-
 import requests
-from bs4 import BeautifulSoup
 
-# 오픈 api사용예정
-# 기본 URL
-url = "https://www.youthcenter.go.kr/opi/youthPlcyList.do"
+def get_youth_policy_data():
+    url = "https://www.youthcenter.go.kr/opi/youthPlcyList.do"
+    params = {
+        'openApiVlak': 'your_api_key',  # API 키를 여기에 입력하세요
+        'pageIndex': 1,
+        'display': 10,  # 한 페이지에 표시할 결과 수
+    }
 
-# 쿼리 파라미터
-params = {
-    'openApiVlak' : '?????', #openapi key
-    'display': '100',
-    'pageIndex': '2',
-    'srchPolyBizSecd': '003002001,003002004,003002008'
-}
+    response = requests.get(url, params=params)
 
-# GET 요청을 보내며 파라미터를 URL에 추가
-response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print("API 요청 실패:", response.status_code)
+        return None
 
-# BeautifulSoup 객체를 생성합니다.
-soup = BeautifulSoup(response.text, 'html.parser')
-
-
-# 각 헤드라인의 텍스트를 출력합니다.
-#for headline in headlines:
-#   print(headline.text.strip())
+data = get_youth_policy_data()
+print(data)
